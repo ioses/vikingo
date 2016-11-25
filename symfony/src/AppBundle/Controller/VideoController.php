de48a5e8100c06f7268910c2b8f29f08ab7e6f8a
ioses <iosesgale@gmail.com>
2016-11-25 13:23:15 +0000

    editVideo function

--- symfony/src/AppBundle/Controller/VideoController.php
+++ symfony/src/AppBundle/Controller/VideoController.php
@@ -99,4 +99,91 @@ class VideoController extends Controller{
     }
     
     
+    public function editAction(Request $request, $video_id = null){
+    
+        $helpers = $this->get("app.helpers");
+        
+        $hash = $request->get("authorization", null);
+        $authCheck = $helpers->authCheck($hash);
+        
+        if($authCheck == true){
+              $identity = $helpers->authCheck($hash, true);
+              
+              $json = $request->get("json", null);
+              
+              if($json!= null){
+                  $params = json_decode($json);
+                  
+                  $createdAt = new \Datetime('now');
+                  $updatedAt = new \Datetime('now');
+                  $image = null;
+                  $video_path = null;
+                  
+                  $user_id = ($identity->sub!= null) ? $identity->sub : null;
+                  $title = (isset($params->title)) ? $params->title : null;
+                  $description = (isset($params->description)) ? $params->description : null;
+                  $status = (isset($params->status)) ? $params->status : null;
+                  
+                  if($user_id != null && $title != null){
+                      $em = $this->getDoctrine()->getManager();
+                      
+                        $video = $em->getRepository("BackendBundle:Video")->findOneBy(
+                            array(
+                                "id" => $video_id
+                                ));
+                        
+                       
+                       if(isset($identity->sub) && $identity->sub == $video->getUser()->getId()){
+                            $video->setTitle($title);
+                            $video->setDescription($description);
+                            $video->setStatus($status);
+                            $video->setUpdatedAt($updatedAt);
+                            
+                            $em->persist($video);
+                            $em->flush();
+                            
+                            $data = array(
+                                "status"=>"success",
+                                "code"=>200,
+                                "data"=>"Video updated successfully!!"
+                            );   
+                       }else{
+                            
+                            $data = array(
+                                "status"=>"error",
+                                "code"=>400,
+                                "data"=>"Video updated error, you are not the owner"
+                            );   
+                       }
+                           
+                    
+                                 
+                  }else{
+                       $data = array(
+                        "status"=>"error",
+                        "code"=>400,
+                        "msg"=>"Video not updated, params failed"
+                        );
+                  }
+                  
+              }else{
+                  $data = array(
+                    "status"=>"error",
+                    "code"=>400,
+                    "msg"=>"Video not updated"
+                    );
+              }
+        }else{
+            $data = array(
+                    "status"=>"error",
+                    "code"=>400,
+                    "msg"=>"Video not valid"
+                    );
+        }
+        
+        return $helpers->json($data);
+    }
+    
+    
+    
 }
